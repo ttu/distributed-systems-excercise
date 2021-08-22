060015a45d0125d0559061a3a35e34a0314381f3
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2021-08-22 09:51:54 +0300

    Remove not done saga files

--- backend-examples/x-saga/paymentService.js
+++
@@ -1,5 +0,0 @@
-export const createPayment = async (order) => {
-  const payload = { amount: order.amount };
-  const response = await axios.post(PAYMENT_URL + '/create-payment', payload);
-  return response.data;
-};
